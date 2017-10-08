cae023b6e86f0e5d624035a90792be5b1957eb19
Chilipp <p.sommer@posteo.de>
2017-10-08 20:57:18 +0200

    Minor fix in classifiers

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name='model-organization',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
         'Operating System :: OS Independent',
       ],
       keywords=('climate model framework'),
